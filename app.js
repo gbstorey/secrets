04b9d2aa16f2f6343885a27b40ea3f56579ad2ca
gbstorey <gbstorey@asu.edu>
2022-07-10 16:33:28 -0700

    db encryption and key security using env

---
+++ app.js
@@ -0,0 +1,75 @@
+// Dependencies
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+ email: String,
+ password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+const User = new mongoose.model("user", userSchema);
+
+app.get("/", function(req, res) {
+ res.render("home");
+});
+
+app.get("/login", function(req, res) {
+ res.render("login");
+});
+
+app.post("/login", function(req, res) {
+ User.findOne({email: req.body.username},
+ function(err, result) {
+   if (err) {
+    console.log(err);
+   } else {
+    if (result) {
+     if (result.password === req.body.password) {
+      res.render("secrets");
+     } else {
+      res.send("Invalid password.");
+     }
+    }
+   }
+  });
+});
+
+
+app.get("/register", function(req, res) {
+ res.render("register");
+});
+
+app.post("/register", function(req, res) {
+ const newUser = new User({
+  email: req.body.username,
+  password: req.body.password
+ });
+ newUser.save(function(err) {
+  if (err) {
+   console.log(err);
+  } else {
+   res.render("secrets");
+  }
+ });
+});
+
+
+
+// Express server start
+app.listen(port, function () {
+ console.log("Server started on port " + port);
+});
